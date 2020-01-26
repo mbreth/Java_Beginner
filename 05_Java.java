c95767a3ba31102d22865a7cea0f0a508311f30a
Morgan Breth <mbreth1@gmail.com>
2020-01-26 15:28:38 -0600

    Create 05_Java.java

---
+++ 05_Java.java
@@ -0,0 +1,11 @@
+// Simple while loop demo
+
+class Main {
+  public static void main(String args[]) {
+    int numCount = 0;
+    while (numCount <= 10) {
+      System.out.println(numCount);
+      numCount = numCount + 1;
+    }
+  }
+}
